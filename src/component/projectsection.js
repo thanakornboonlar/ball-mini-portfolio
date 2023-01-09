a3277dfc98ae04c42d98d353b48da7f385bfa7e2
BestBall <thanakorn.boonlar@gmail.com>
2023-01-09 15:43:45 +0700

    fix:fix project header

--- src/component/projectsection.js
+++ src/component/projectsection.js
@@ -4,8 +4,8 @@ function ProjectSection() {
   return (
     <div className="passion-section">
       <div className="title">
-        <h1 id="first-word">Additional</h1>
-        <h1 id="second-word">Passions</h1>
+        {/* <h1 id="first-word">Additional</h1> */}
+        <h1 id="second-word">Projects</h1>
       </div>
       <div className="passion-container">
         <a
